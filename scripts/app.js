7e9f4fe5c4db139f5ea1975c2effc56c71ba15a0
Masakazu Segawa <segawm@MSegawas-Home-Mac.local>
2018-09-28 05:48:43 +0900

    display formatted time

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,11 @@
 // Web APIで現在時刻を取得
 // 日本標準時プロジェクト http://www.nict.go.jp/JST/http.html
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://ntp-a1.nict.go.jp/cgi-bin/time');
+xhr.open('GET', 'https://ntp-a1.nict.go.jp/cgi-bin/json');
 xhr.onreadystatechange = function() {
   if (xhr.readyState == 4 && xhr.status == 200) {
-    document.getElementById('time').innerHTML = xhr.responseText;
+    var date = new Date(JSON.parse(xhr.responseText).st * 1000);
+    document.getElementById('time').innerHTML = date.toLocaleString();
   }
 };
 xhr.send();
